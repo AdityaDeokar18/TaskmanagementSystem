327af52d23e8460fe263fe98a8e3f83ee3657f0e
AdityaDeokar18 <adityadeokar0007@gmail.com>
2026-02-12 16:02:21 +0530

    Add files via upload

---
+++ src/main/java/com/TaskmanagementSystem/Enum/IssueStatus.java
@@ -0,0 +1,3 @@
+
+package com.TaskmanagementSystem.Enum;
+public enum IssueStatus { TODO, IN_PROGRESS, DONE }
---
+++ src/main/java/com/TaskmanagementSystem/Enum/IssueType.java
@@ -0,0 +1,3 @@
+
+package com.TaskmanagementSystem.Enum;
+public enum IssueType { BUG, TASK, STORY }
