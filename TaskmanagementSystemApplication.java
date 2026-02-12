701069277a6b30aa6ca85512a47e5e3c9ec1d5f4
AdityaDeokar18 <adityadeokar0007@gmail.com>
2026-02-12 15:55:46 +0530

    Add files via upload

---
+++ TaskmanagementSystemApplication.java
@@ -0,0 +1,12 @@
+
+package com.TaskmanagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagementSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagementSystemApplication.class, args);
+    }
+}
